aadf8675727f6cb3ea66e930c94bda62485d8ecb
Wade Rance <wrance97@gmail.com>
2018-09-06 19:39:50 -0500

    Removed unused import statements

--- crawler.py
+++ crawler.py
@@ -1,21 +1,10 @@
 from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.support.ui import Select
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.common.exceptions import TimeoutException
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.common.exceptions import NoSuchElementException
-from selenium.common.exceptions import NoAlertPresentException
 from selenium.webdriver.firefox.options import Options
 
 import sys
-import unittest, time, re
+import time
 from bs4 import BeautifulSoup as bs
-from dateutil import parser
 import pandas as pd
-import itertools
-import matplotlib.pyplot as plt
 
 username = "USERNAME"
 password = "PSWD"
