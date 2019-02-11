cd50f1da65fca4d9d461842a38e444baccfaecf2
Wade Rance <wade@Wades-MacBook-Air.local>
2019-02-11 00:44:07 +0000

    Started Layouts

---
+++ Whisk/Whisk/LoginViewController.swift
@@ -0,0 +1,17 @@
+//
+//  LoginViewController.swift
+//  Whisk
+//
+//  Created by Bryant Le on 2/9/19.
+//  Copyright © 2019 BryantLe. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
--- Whisk/Whisk/User.swift
+++ Whisk/Whisk/User.swift
@@ -6,36 +6,36 @@
 //  Copyright © 2019 BryantLe. All rights reserved.
 //
 
-import UIKit
-
-class User {
-    // MARK: Properties
-    
-    var firstName: String
-    var lastName: String
-    var location: [Double]
-    var radius: Int
-    var gender: Bool
-    var preference: [Bool]
-    var photos: [UIImage]
-    // var vector: VECTOR CLASS
-    
-    // MARK: Initialization
-    
-    init?(name: String, photo: UIImage?, rating: Int) {
-        // The name must not be empty
-        guard !name.isEmpty else {
-            return nil
-        }
-        
-        // The rating must be between 0 and 5 inclusively
-        guard (rating >= 0) && (rating <= 5) else {
-            return nil
-        }
-        
-        self.name = name
-        self.photo = photo
-        self.rating = rating
-
-    }
-}
+//import UIKit
+//
+//class User {
+//    // MARK: Properties
+//
+//    var firstName: String
+//    var lastName: String
+//    var location: [Double]
+//    var radius: Int
+//    var gender: Bool
+//    var preference: [Bool]
+//    var photos: [UIImage]
+//    // var vector: VECTOR CLASS
+//
+//    // MARK: Initialization
+//
+//    init?(name: String, photo: UIImage?, rating: Int) {
+//        // The name must not be empty
+//        guard !name.isEmpty else {
+//            return nil
+//        }
+//
+//        // The rating must be between 0 and 5 inclusively
+//        guard (rating >= 0) && (rating <= 5) else {
+//            return nil
+//        }
+//
+//        self.name = name
+//        self.photo = photo
+//        self.rating = rating
+//
+//    }
+//}
